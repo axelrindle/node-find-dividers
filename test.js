c9559c1bd71df78335904919fdb4c46761745259
Axel Rindle <axel.rindle@gmx.de>
2018-02-08 17:44:42 +0100

    Tweak output

--- test.js
+++ test.js
@@ -2,14 +2,15 @@ const dividers = require('./');
 
 const list = [20, 2, 100, 33, 1, 44, 2];
 
+console.log();
 console.log("1 excluded:");
 for (n of list) {
   let d = dividers(n, true);
-  console.log("[" + d + "]" + "  =>  " + d.length);
+  console.log("%s: [%s] => %s", n, d, d.length);
 }
 console.log();
 console.log("1 included:");
 for (n of list) {
   let d = dividers(n);
-  console.log("[" + d + "]" + "  =>  " + d.length);
+  console.log("%s: [%s] => %s", n, d, d.length);
 }
